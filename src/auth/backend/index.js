ed8f8a7ef1faf2a4a20ab1f6d78e38bae6ea9539
madya135 <adtsymadyab@gmail.com>
2023-06-16 18:18:47 +0800

    update

--- src/auth/backend/index.js
+++ src/auth/backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import db from "./config/database.js";
+import db from "./config/Database.js";
 import router from "./routes/index.js";
 // import Users from "./models/usermodels.js"; // Untuk membuat tabel jika belum mebuat
 dotenv.config();
